c46e91c3999975061b93d00a7f60b7280809724f
nickdip <51235112+nickdip@users.noreply.github.com>
2023-08-04 14:36:29 +0000

    Creating test search page

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect
+from flask_session import Session
+from tempfile import mkdtemp
 
 app = Flask(__name__)
 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return render_template("search.html")
